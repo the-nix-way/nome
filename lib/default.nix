ed8cc95aa911b66097d63feb7b54eab74861fbf9
Luc Perkins <lucperkins@gmail.com>
2022-08-19 14:17:41 +0300

    Split toolchains into separate directory

--- lib/default.nix
+++ lib/default.nix
@@ -22,40 +22,5 @@ in rec {
     });
 
   # The toolchains that I commonly use
-  toolchains = {
-    devops = with pkgs; [ flyctl packer terraform vagrant ];
-
-    elixir = let
-      darwinDeps = darwinOnly ((with pkgs; [ terminal-notifier ])
-        ++ (with pkgs.darwin.apple_sdk.frameworks; [
-          CoreFoundation
-          CoreServices
-        ]));
-      linuxDeps = linuxOnly (with pkgs; [ inotify-tools libnotify ]);
-    in with pkgs; [ elixir ] ++ darwinDeps ++ linuxDeps;
-
-    go = with pkgs; [ go go2nix gotools ];
-
-    kubernetes = with pkgs; [ kubectl kubectx kustomize minikube ];
-
-    node = with pkgs; [ nodejs yarn ] ++ (with pkgs.nodePackages; [ pnpm ]);
-
-    protobuf = with pkgs; [ buf protobuf ];
-
-    rust = with pkgs; [
-      devRust
-      cargo-audit
-      cargo-cross
-      cargo-deny
-      cargo-edit
-      cargo-expand
-      cargo-fuzz
-      cargo-make
-      cargo-outdated
-      cargo-profiler
-      openssl
-      pkg-config
-      rust-analyzer
-    ];
-  };
+  toolchains = import ./toolchains { inherit darwinOnly linuxOnly pkgs; };
 }
