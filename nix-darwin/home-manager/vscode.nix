ed35c82df05ec16cfeda0665ff0e3255199d1a33
Luc Perkins <lucperkins@gmail.com>
2023-11-28 18:51:58 +0300

    Switch VS Code theme

--- nix-darwin/home-manager/vscode.nix
+++ nix-darwin/home-manager/vscode.nix
@@ -3,7 +3,7 @@
 let
   font = "CascadiaCode";
   terminalFont = "Iosevka";
-  colorTheme = "GitHub Dark Dimmed";
+  colorTheme = "GitHub Dark Mode";
   iconTheme = "material-icon-theme";
 
   ext = publisher: name: version: sha256: pkgs.vscode-utils.buildVscodeMarketplaceExtension {
@@ -21,14 +21,17 @@ in
     esbenp.prettier-vscode
     github.vscode-github-actions
     golang.go
+    #markusylisiurunen.githubdarkmode
     rust-lang.rust-analyzer
     tamasfe.even-better-toml
 
     # Extensions not in Nixpkgs
     (ext "b4dM4n" "nixpkgs-fmt" "0.0.1" "sha256-vz2kU36B1xkLci2QwLpl/SBEhfSWltIDJ1r7SorHcr8=")
     (ext "GitHub" "github-vscode-theme" "6.3.4" "sha256-JbI0B7jxt/2pNg/hMjAE5pBBa3LbUdi+GF0iEZUDUDM=")
+    (ext "markusylisiurunen" "githubdarkmode" "0.1.6" "sha256-Xzh8g5bEi4kPul1nJyROcN0CeDnXuNxQEYt6HgMepvM=")
     (ext "ms-vscode" "vscode-typescript-next" "5.4.20231127" "sha256-UVuYggzeWyQTmQxXdM4sT78FUOtYGKD4SzREntotU5g=")
     (ext "PKief" "material-icon-theme" "4.32.0" "sha256-6I9/nWv449PgO1tHJbLy/wxzG6BQF6X550l3Qx0IWpw=")
+    (ext "whizkydee" "material-palenight-theme" "2.0.3" "sha256-qz2pz9JlnO2OV3eJnRqzbcic1lzpl0ViygwhNjZOWpI=")
   ];
 
   globalSnippets = { };
