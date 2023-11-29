436c1866a6eb877264d3c9a92a37c341c6594cbe
Luc Perkins <lucperkins@gmail.com>
2023-11-29 20:04:13 +0300

    Move macOS configs to separate file

---
+++ nix-darwin/base/macos.nix
@@ -0,0 +1,103 @@
+{ pkgs }:
+
+{
+  configurationRevision = pkgs.rev;
+
+  defaults = {
+    dock = {
+      appswitcher-all-displays = false;
+      autohide = true;
+      autohide-delay = 0.5;
+      autohide-time-modifier = 1.0;
+      dashboard-in-overlay = false;
+      enable-spring-load-actions-on-all-items = false;
+      expose-animation-duration = 1.0;
+      expose-group-by-app = true;
+      largesize = 16;
+      launchanim = true;
+      magnification = false;
+      mineffect = "genie";
+      minimize-to-application = false;
+      mouse-over-hilite-stack = null;
+      mru-spaces = true;
+      orientation = "bottom";
+      show-process-indicators = true;
+      show-recents = true;
+      showhidden = true;
+      static-only = false;
+      tilesize = 64;
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
+    };
+
+    finder = {
+      AppleShowAllExtensions = true;
+      FXEnableExtensionChangeWarning = false;
+      QuitMenuItem = true;
+    };
+
+    NSGlobalDomain = {
+      AppleEnableMouseSwipeNavigateWithScrolls = true;
+      AppleEnableSwipeNavigateWithScrolls = true;
+      AppleFontSmoothing = 2;
+      AppleICUForce24HourTime = true;
+      AppleInterfaceStyle = "Dark";
+      AppleInterfaceStyleSwitchesAutomatically = false;
+      AppleKeyboardUIMode = 3;
+      AppleMeasurementUnits = "Inches";
+      AppleMetricUnits = 0;
+      ApplePressAndHoldEnabled = true;
+      AppleScrollerPagingBehavior = false;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      AppleShowScrollBars = "WhenScrolling";
+      AppleTemperatureUnit = "Celsius";
+      AppleWindowTabbingMode = "fullscreen";
+      InitialKeyRepeat = 11;
+      KeyRepeat = 1;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSAutomaticWindowAnimationsEnabled = true;
+      NSDisableAutomaticTermination = false;
+      NSDocumentSaveNewDocumentsToCloud = true;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      NSScrollAnimationEnabled = true;
+      NSTableViewDefaultSizeMode = 2;
+      NSTextShowsControlCharacters = true;
+      NSUseAnimatedFocusRing = true;
+      NSWindowResizeTime = 0.2;
+      PMPrintingExpandedStateForPrint = false;
+      PMPrintingExpandedStateForPrint2 = false;
+      _HIHideMenuBar = true;
+
+      "com.apple.keyboard.fnState" = false;
+      "com.apple.mouse.tapBehavior" = null;
+      "com.apple.sound.beep.feedback" = 0;
+      "com.apple.sound.beep.volume" = 0.0;
+      "com.apple.springing.delay" = 0.5;
+      "com.apple.springing.enabled" = false;
+      "com.apple.swipescrolldirection" = true;
+      "com.apple.trackpad.enableSecondaryClick" = true;
+      "com.apple.trackpad.scaling" = 1.0;
+      "com.apple.trackpad.trackpadCornerClickBehavior" = null;
+    };
+
+    SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
+
+    trackpad = {
+      Clicking = true;
+      TrackpadThreeFingerDrag = true;
+    };
+  };
+
+  keyboard = {
+    enableKeyMapping = true;
+    remapCapsLockToControl = true;
+  };
+}
