d9b6bde1147ca000bc69e12258f42a71d56e2c3c
Luc Perkins <lucperkins@gmail.com>
2025-05-21 16:04:17 -0400

    Update flake.lock

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -5,7 +5,7 @@ let
   terminalFont = "JetBrains Mono";
   iconTheme = "material-icon-theme";
 
-  ext = publisher: name: version: sha256: pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+  vsce = publisher: name: version: sha256: pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     mktplcRef = { inherit name publisher sha256 version; };
   };
 in
@@ -48,14 +48,14 @@ in
     yzhang.markdown-all-in-one
   ]) ++ [
     # Extensions not in Nixpkgs
-    (ext "andrejunges" "Handlebars" "0.4.1" "sha256-Rwhr9X3sjDm6u/KRYE2ucCJSlZwsgUJbH/fdq2WZ034=")
-    (ext "antfu" "theme-vitesse" "0.8.3" "sha256-KkpJgJBcnMeQ1G97QS/E6GY4/p9ebZRaA5pUXPd9JB0=")
-    (ext "bufbuild" "vscode-buf" "0.7.0" "sha256-B5/Gc+f3xaYpMTXFcQ9LJiAb9LBJX2aR+gh22up3Wi4=")
-    (ext "cuelangorg" "vscode-cue" "0.0.9" "sha256-aq+O0bXc9a5namjqqah+samZCF2xKFlK0HBcA3hmxIg=")
-    (ext "oven" "bun-vscode" "0.0.26" "sha256-klMkKAorWJj2o015FWbQQfpmYe4JM0UOM+WVh+YPtI4=")
-    (ext "Guyutongxue" "lalrpop-syntax-highlight" "0.0.5" "sha256-VJBvR9pM0NPYi/RUoVQcL1tt2PZCKohwX8Dd1nz0UGY=")
-    (ext "markusylisiurunen" "githubdarkmode" "0.1.6" "sha256-Xzh8g5bEi4kPul1nJyROcN0CeDnXuNxQEYt6HgMepvM=")
-    (ext "ms-vscode" "vscode-typescript-next" "5.4.20231127" "sha256-UVuYggzeWyQTmQxXdM4sT78FUOtYGKD4SzREntotU5g=")
+    (vsce "andrejunges" "Handlebars" "0.4.1" "sha256-Rwhr9X3sjDm6u/KRYE2ucCJSlZwsgUJbH/fdq2WZ034=")
+    (vsce "antfu" "theme-vitesse" "0.8.3" "sha256-KkpJgJBcnMeQ1G97QS/E6GY4/p9ebZRaA5pUXPd9JB0=")
+    (vsce "bufbuild" "vscode-buf" "0.7.0" "sha256-B5/Gc+f3xaYpMTXFcQ9LJiAb9LBJX2aR+gh22up3Wi4=")
+    (vsce "cuelangorg" "vscode-cue" "0.0.9" "sha256-aq+O0bXc9a5namjqqah+samZCF2xKFlK0HBcA3hmxIg=")
+    (vsce "oven" "bun-vscode" "0.0.26" "sha256-klMkKAorWJj2o015FWbQQfpmYe4JM0UOM+WVh+YPtI4=")
+    (vsce "Guyutongxue" "lalrpop-syntax-highlight" "0.0.5" "sha256-VJBvR9pM0NPYi/RUoVQcL1tt2PZCKohwX8Dd1nz0UGY=")
+    (vsce "markusylisiurunen" "githubdarkmode" "0.1.6" "sha256-Xzh8g5bEi4kPul1nJyROcN0CeDnXuNxQEYt6HgMepvM=")
+    (vsce "ms-vscode" "vscode-typescript-next" "5.4.20231127" "sha256-UVuYggzeWyQTmQxXdM4sT78FUOtYGKD4SzREntotU5g=")
   ];
 
   globalSnippets = { };
