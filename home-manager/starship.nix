f37ef18e0107e834e770dc9a9a282ed2b03c2bbc
Luc Perkins <lucperkins@gmail.com>
2025-08-25 18:32:25 +0100

    Update starship theme

--- home-manager/starship.nix
+++ home-manager/starship.nix
@@ -12,8 +12,8 @@
         pkgs.fetchFromGitHub {
           owner = "catppuccin";
           repo = "starship";
-          rev = "e99ba6b210c0739af2a18094024ca0bdf4bb3225";
-          sha256 = "sha256-1w0TJdQP5lb9jCrCmhPlSexf0PkAlcz8GBDEsRjPRns=";
+          rev = "5906cc369dd8207e063c0e6e2d27bd0c0b567cb8";
+          sha256 = "sha256-FLHjbClpTqaK4n2qmepCPkb8rocaAo3qeV4Zp1hia0g=";
         }
       }/themes/mocha.toml"
     ))
